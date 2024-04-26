f2b0d458042e78719a7822ce6e04d5b6e269ae49
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-04-26 22:43:31 +0800

    Update help text

--- src/components/help-dialog.tsx
+++ src/components/help-dialog.tsx
@@ -14,13 +14,16 @@ const HelpDialog = () => {
       <DialogTrigger asChild>
         <Button variant="link">Clockify Requirements</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent>
         <DialogHeader>
-          <DialogTitle>Logging Time Correctly</DialogTitle>
-          <DialogDescription>
-            To ensure the timesheet is as accurate as possible, ensure you do
-            the following.
-          </DialogDescription>
+          <div className="space-y-4">
+            <DialogTitle>Logging Time Correctly</DialogTitle>
+            <DialogDescription>
+              This tool expects your time entries to be in an exact format. To
+              avoid incorrect or missing time entries, please ensure you do the
+              following.
+            </DialogDescription>
+          </div>
         </DialogHeader>
         <div className="space-y-2">
           <p className="font-medium">All Time Entries</p>
@@ -28,7 +31,7 @@ const HelpDialog = () => {
             <li>Enter a detailed description</li>
             <li>Select a project</li>
           </ul>
-          <p className="font-medium">Paid Client Work Only</p>
+          <p className="font-medium">Paid Client Work</p>
           <ul className="text-sm list-disc px-4">
             <li>Click the billable icon - $</li>
             <li>
