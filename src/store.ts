afe4d4fa9fe79cbbf0d08f5851473a3e0133c4b5
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-04-29 20:20:51 +0800

    Clean up order of properties

--- src/store.ts
+++ src/store.ts
@@ -4,43 +4,46 @@ import { persist } from "zustand/middleware";
 
 type UserStore = {
   name: string;
-  setName: (newName: string) => void;
   userId: string;
-  setUserId: (newUserId: string) => void;
   resource: string;
-  setResource: (newResource: string) => void;
   callNo: string;
-  setCallNo: (newCallNo: string) => void;
   workspaceId: string;
-  setWorkspaceId: (newWorkspaceId: string) => void;
   apiKey: string;
-  setApiKey: (newApiKey: string) => void;
   projects: ProjectType[];
-  setProjects: (newProjects: ProjectType[]) => void;
   prefersProjectName: boolean;
+  setName: (newName: string) => void;
+  setUserId: (newUserId: string) => void;
+  setResource: (newResource: string) => void;
+  setCallNo: (newCallNo: string) => void;
+  setWorkspaceId: (newWorkspaceId: string) => void;
+  setApiKey: (newApiKey: string) => void;
+  setProjects: (newProjects: ProjectType[]) => void;
   setPrefersProjectName: (newPreference: boolean) => void;
 };
 
 export const useUserStore = create<UserStore>()(
   persist(
     (set) => ({
-  name: "",
-  setName: (newName) => set({ name: newName }),
-  userId: "",
-  setUserId: (newUserId) => set({ userId: newUserId }),
-  resource: "",
-  setResource: (newResource) => set({ resource: newResource }),
-  callNo: "",
-  setCallNo: (newCallNo) => set({ callNo: newCallNo }),
-  workspaceId: "",
-  setWorkspaceId: (newWorkspaceId) => set({ workspaceId: newWorkspaceId }),
-  apiKey: "",
-  setApiKey: (newApiKey) => set({ apiKey: newApiKey }),
-  projects: [],
-  setProjects: (newProjects) => set({ projects: newProjects }),
-  prefersProjectName: false,
-  setPrefersProjectName: (newPreference) =>
-  set({ prefersProjectName: newPreference }),
-}), {
-  name: "user",
-}));
+      name: "",
+      userId: "",
+      resource: "",
+      callNo: "",
+      workspaceId: "",
+      apiKey: "",
+      projects: [],
+      prefersProjectName: false,
+      setName: (newName) => set({ name: newName }),
+      setUserId: (newUserId) => set({ userId: newUserId }),
+      setResource: (newResource) => set({ resource: newResource }),
+      setCallNo: (newCallNo) => set({ callNo: newCallNo }),
+      setWorkspaceId: (newWorkspaceId) => set({ workspaceId: newWorkspaceId }),
+      setApiKey: (newApiKey) => set({ apiKey: newApiKey }),
+      setProjects: (newProjects) => set({ projects: newProjects }),
+      setPrefersProjectName: (newPreference) =>
+        set({ prefersProjectName: newPreference }),
+    }),
+    {
+      name: "user",
+    }
+  )
+);
