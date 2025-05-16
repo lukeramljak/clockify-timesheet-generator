81bd3f0f0e4cd00d8cd882d23ca7ba55807543de
Luke Ramljak <lukeramljak@proton.me>
2025-05-16 18:08:25 +0800

    deps: add tailwind vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     environment: "jsdom",
     globals: true,
